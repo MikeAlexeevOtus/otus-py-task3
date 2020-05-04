7652e134a32232030c5478ceab495643c1f1f4c0
Mikhail Alexeev <mikhail.alexeev@kaspersky.com>
2020-05-04 13:55:28 +0300

    refactored validation

--- request_object.py
+++ request_object.py
@@ -32,27 +32,26 @@ class Field(object):
         return self.val
 
     def __set__(self, obj, val):
+        self._validate(val)
+        self.val = val
+
+    def _validate(self, val):
         if val is None and not self.nullable:
             raise ValueError('field "{}" can\'t be null'.format(self.name))
-        self.val = val
 
 
 class DictField(Field):
-    def __set__(self, obj, val):
-        super(DictField, self).__set__(obj, val)
+    def _validate(self, val):
+        super(DictField, self)._validate(val)
         if val is not None and not isinstance(val, dict):
             raise ValueError('field "{}" must be a dict'.format(self.name))
 
-        self.val = val
-
 
 class CharField(Field):
-    def __set__(self, obj, val):
-        super(CharField, self).__set__(obj, val)
+    def _validate(self, val):
+        super(CharField, self)._validate(val)
         if val is not None and not isinstance(val, basestring):
             raise ValueError('field "{}" must be a string'.format(self.name))
-
-        self.val = val
 
 
 class ArgumentsField(DictField):
@@ -68,14 +67,12 @@ class PhoneField(object):
 
 
 class DateField(CharField):
-    def __set__(self, obj, val):
-        super(DateField, self).__set__(obj, val)
+    def _validate(self, val):
+        super(DateField, self)._validate(val)
         try:
             datetime.datetime.strptime(val, '%d.%m.%Y')
         except ValueError:
             raise ValueError('field "{}" must be a string in format "DD.MM.YYYY"'.format(self.name))
-
-        self.val = val
 
 
 class BirthDayField(object):
@@ -83,8 +80,8 @@ class BirthDayField(object):
 
 
 class GenderField(Field):
-    def __set__(self, obj, val):
-        super(GenderField, self).__set__(obj, val)
+    def _validate(self, val):
+        super(GenderField, self)._validate(val)
         possible_values = sorted(GENDERS.keys())
         err = 'field "{}" must be an integer, one of {}'.format(
             self.name,
@@ -96,12 +93,10 @@ class GenderField(Field):
         elif val not in possible_values:
             raise ValueError(err)
 
-        self.val = val
-
 
 class ClientIDsField(Field):
-    def __set__(self, obj, val):
-        super(ClientIDsField, self).__set__(obj, val)
+    def _validate(self, val):
+        super(ClientIDsField, self)._validate(val)
         err = 'field "{}" must be a list of integers'.format(self.name)
         if val is not None and not isinstance(val, list):
             raise ValueError(err)
@@ -109,8 +104,6 @@ class ClientIDsField(Field):
         for id_ in val:
             if not isinstance(id_, int):
                 raise ValueError(err)
-
-        self.val = val
 
 
 class ClientsInterestsRequest(object):
