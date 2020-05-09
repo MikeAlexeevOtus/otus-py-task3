534302185b0aec4d0bd8f918a2a91b13494e5316
Mikhail Alexeev <mikhail.alexeev@kaspersky.com>
2020-05-09 13:51:09 +0300

    added tests for email field

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -5,6 +5,7 @@ import unittest
 from request_object import (
     Field,
     CharField,
+    EmailField,
     ValidationError,
 )
 from utils import cases
@@ -13,7 +14,8 @@ from utils import cases
 class TestFields(unittest.TestCase):
     @cases([
         Field,
-        CharField
+        CharField,
+        EmailField
     ])
     def test_nullable(self, cls):
         field = cls(nullable=True)
@@ -28,8 +30,20 @@ class TestFields(unittest.TestCase):
         field = CharField(nullable=True)
         field._validate(value)
 
+    @cases([CharField, EmailField])
     @cases([0, 42, {}, []])
-    def test_charfield_forbidden(self, value):
-        field = CharField(nullable=True)
+    def test_charfield_forbidden(self, cls, value):
+        field = EmailField()
         with self.assertRaisesRegexp(ValidationError, "must be a string"):
+            field._validate(value)
+
+    @cases([None, 'abcd@abcd', u'фыва@фыва'])
+    def test_email_allowed(self, value):
+        field = EmailField(nullable=True)
+        field._validate(value)
+
+    @cases(['abcd', u'фыва', '', u''])
+    def test_email_forbidden(self, value):
+        field = EmailField()
+        with self.assertRaisesRegexp(ValidationError, "must be a valid email addr"):
             field._validate(value)
