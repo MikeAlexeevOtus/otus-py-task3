9573304f3b6dc09f2bd6ac69b9ed58d253e82a71
Mikhail Alexeev <mikhail.alexeev@kaspersky.com>
2020-05-09 13:17:12 +0300

    added charfield validation tests

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,8 +1,10 @@
+#! encoding: utf-8
 import unittest
 
 
 from request_object import (
     Field,
+    CharField,
     ValidationError,
 )
 from utils import cases
@@ -16,7 +18,21 @@ class TestFields(unittest.TestCase):
         with self.assertRaisesRegexp(ValidationError, "can't be null"):
             field._validate(None)
 
-    def test_nullable_allowed(self):
+    @cases([
+        0,  # any not-None value
+        None
+    ])
+    def test_nullable_allowed(self, value):
         field = Field(nullable=True)
-        field._validate(0)   # any not-None value
-        field._validate(None)
+        field._validate(value)
+
+    @cases([None, '', u'', 'abc', u'фыва'])
+    def test_charfield_allowed(self, value):
+        field = CharField(nullable=True)
+        field._validate(value)
+
+    @cases([0, 42, {}, []])
+    def test_charfield_forbidden(self, value):
+        field = CharField(nullable=True)
+        with self.assertRaisesRegexp(ValidationError, "must be a string"):
+            field._validate(value)
